b05cc2a6bd7b1216effbacbfcbc7d64c77344e83
Szegedi Barnabás <szeveba@gmail.com>
2023-12-04 17:01:33 +0100

    Dokumentációs kommentekkel kiegészítve

--- Ultrabalaton/Program.cs
+++ Ultrabalaton/Program.cs
@@ -2,6 +2,11 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// Bementi adatok előfeldolgozása a későbbi optimálisabb munka érdekében.
+        /// </summary>
+        /// <param name="fájlnév">Relatív elérési út</param>
+        /// <returns>Bemeneti adatok feldarabolva, fejléc nélkül</returns>
         static string[][] FájlBeolvasás(string fájlnév)
         {
             var lines = File.ReadAllLines(fájlnév);
@@ -12,11 +17,19 @@
             }
             return op;
         }
+        /// <summary>
+        /// Határozza meg és írja ki a képernyőre a minta szerint, hogy hány egyéni sportoló indult el a versenyen! 
+        /// </summary>
         static void Feladat3(string[][] adatok)
         {
             int indulókSzáma = adatok.Length;
             MegoldásKiiratás(3, $"Egyéni indulók: {indulókSzáma} fő");
         }
+        /// <summary>
+        /// Feladat megoldások kiíratására.
+        /// </summary>
+        /// <param name="feladatSorszám">Feladat sorszáma</param>
+        /// <param name="megoldás">A megoldás szövege</param>
         static void MegoldásKiiratás(int feladatSorszám, string megoldás)
         {
             Console.WriteLine($"{feladatSorszám}. feladat: {megoldás}");
