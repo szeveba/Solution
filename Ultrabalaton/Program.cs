26162c4ecd58f9eb81e0ff209cf6dd4372400bbd
Szegedi Barnabás <szeveba@gmail.com>
2023-12-04 18:24:20 +0100

    Végső refaktorálás, kész a feladat

--- Ultrabalaton/Program.cs
+++ Ultrabalaton/Program.cs
@@ -7,12 +7,21 @@
         const int KategóriaIndex = 2;
         const int VersenyIdőIndex = 3;
         const int TávSzázalékIndex = 4;
+        static void Main(string[] args)
+        {
+            var adatok = FájlBeolvasás("ub2017egyeni.txt");
+            Feladat3(adatok);
+            Feladat4(adatok);
+            Feladat5(adatok);
+            Feladat7(adatok);
+            Feladat8(adatok);
+        }
         /// <summary>
         /// Feladat megoldások kiíratására.
         /// </summary>
         /// <param name="feladatSorszám">Feladat sorszáma</param>
         /// <param name="megoldás">A megoldás szövege</param>
-        static void MegoldásKiiratás(int feladatSorszám, string megoldás)
+        private static void MegoldásKiiratás(int feladatSorszám, string megoldás)
         {
             Console.WriteLine($"{feladatSorszám}. feladat: {megoldás}");
         }
@@ -21,7 +30,7 @@
         /// </summary>
         /// <param name="fájlnév">Relatív elérési út</param>
         /// <returns>Bemeneti adatok feldarabolva, fejléc nélkül</returns>
-        static string[][] FájlBeolvasás(string fájlnév)
+        private static string[][] FájlBeolvasás(string fájlnév)
         {
             var lines = File.ReadAllLines(fájlnév);
             var op = new string[lines.Length - 1][];
@@ -34,7 +43,7 @@
         /// <summary>
         /// Határozza meg és írja ki a képernyőre a minta szerint, hogy hány egyéni sportoló indult el a versenyen! 
         /// </summary>
-        static void Feladat3(string[][] adatok)
+        private static void Feladat3(string[][] adatok)
         {
             int indulókSzáma = adatok.Length;
             MegoldásKiiratás(3, $"Egyéni indulók: {indulókSzáma} fő");
@@ -55,52 +64,6 @@
             }
             MegoldásKiiratás(4, $"Célba érkező női sportolók: {db} fő");
         }
-        static void Main(string[] args)
-        {
-            var adatok = FájlBeolvasás("ub2017egyeni.txt");
-            Feladat3(adatok);
-            Feladat4(adatok);
-            Feladat5(adatok);
-            Feladat7(adatok);
-            Feladat8(adatok);
-        }
-
-        /// <summary>
-        /// Keresse meg a női és a férfi kategóriák győzteseit és írja ki nevüket, rajtszámukat és időeredményeiket a minta szerint! Feltételezheti, hogy egyik kategóriában sem alakult ki oltverseny és mindkét kategóriában volt célba érkező futó.
-        /// </summary>
-        private static void Feladat8(string[][] adatok)
-        {
-            //kiindulási alap: maximumkiválasztás programozási tétel, szerintem ezt a megoldást jobban végiggondoltam mint az aki kitalálta a feladatot, hiába azonos szerkezetű bemeneti fájl, kivételes esetben ennél egyszerűbb megoldás hibás eredményt adna (de attól még járna amúgy a max pontszám valószínűleg)
-            int nőiMaxIdx = -1;
-            int férfiMaxIdx = -1;
-            for (int i = 0; i < adatok.Length; i++)
-            {
-                string[] adatsor = adatok[i];
-                if (adatsor[TávSzázalékIndex] == "100")
-                {
-                    if (adatsor[KategóriaIndex] == "Noi")
-                    {
-                        if (nőiMaxIdx == -1 || IdőÓrában(adatsor[VersenyIdőIndex]) < IdőÓrában(adatok[nőiMaxIdx][VersenyIdőIndex]))
-                        {
-                            nőiMaxIdx = i;
-                        }
-                    }
-                    else
-                    {
-                        if (férfiMaxIdx == -1 || IdőÓrában(adatsor[VersenyIdőIndex]) < IdőÓrában(adatok[férfiMaxIdx][VersenyIdőIndex]))
-                        {
-                            férfiMaxIdx = i;
-                        }
-                    }
-                }
-            }
-            MegoldásKiiratás(8,
-                $"Verseny győztesei\n" +
-                $"\tNők: {adatok[nőiMaxIdx][NévIndex]} ({adatok[nőiMaxIdx][RajtszámIndex]}.) - {adatok[nőiMaxIdx][VersenyIdőIndex]}\n" +
-                $"\tFérfiak {adatok[férfiMaxIdx][NévIndex]} ({adatok[férfiMaxIdx][RajtszámIndex]}.) - {adatok[férfiMaxIdx][VersenyIdőIndex]}");
-        }
-
-
         /// <summary>
         /// Kérje be a felhasználótól egy sportoló nevét, majd határozza meg és írja ki a minta szerint, hogy a sportoló indult-e a versenyen! A keresést ne folytassa, ha az eredményt meg tudja határozni! Ha a sportoló indult a versenyen, akkor azt is írja ki a képernyőre, hogy a teljes távot teljesítette-e! Feltételezheti, hogy nem indultak azonos nevű sportolók ezen a versenyen.         
         /// </summary>
@@ -165,6 +128,40 @@
             // A feladat szerint feltételezhetem, hogy lesz legalább egy a feltételt teljesítő sportoló, így nem kell aggódni a 0-val való osztás miatt.
             double átlag = Math.Round(szum / db, 12);
             MegoldásKiiratás(7, $"Átlagos idő: {átlag} óra");
+        }
+        /// <summary>
+        /// Keresse meg a női és a férfi kategóriák győzteseit és írja ki nevüket, rajtszámukat és időeredményeiket a minta szerint! Feltételezheti, hogy egyik kategóriában sem alakult ki oltverseny és mindkét kategóriában volt célba érkező futó.
+        /// </summary>
+        private static void Feladat8(string[][] adatok)
+        {
+            //kiindulási alap: maximumkiválasztás programozási tétel, szerintem ezt a megoldást jobban végiggondoltam mint az aki kitalálta a feladatot, hiába azonos szerkezetű bemeneti fájl, kivételes esetben ennél egyszerűbb megoldás hibás eredményt adna (de attól még járna amúgy a max pontszám valószínűleg)
+            int nőiMaxIdx = -1;
+            int férfiMaxIdx = -1;
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string[] adatsor = adatok[i];
+                if (adatsor[TávSzázalékIndex] == "100")
+                {
+                    if (adatsor[KategóriaIndex] == "Noi")
+                    {
+                        if (nőiMaxIdx == -1 || IdőÓrában(adatsor[VersenyIdőIndex]) < IdőÓrában(adatok[nőiMaxIdx][VersenyIdőIndex]))
+                        {
+                            nőiMaxIdx = i;
+                        }
+                    }
+                    else
+                    {
+                        if (férfiMaxIdx == -1 || IdőÓrában(adatsor[VersenyIdőIndex]) < IdőÓrában(adatok[férfiMaxIdx][VersenyIdőIndex]))
+                        {
+                            férfiMaxIdx = i;
+                        }
+                    }
+                }
+            }
+            MegoldásKiiratás(8,
+                $"Verseny győztesei\n" +
+                $"\tNők: {adatok[nőiMaxIdx][NévIndex]} ({adatok[nőiMaxIdx][RajtszámIndex]}.) - {adatok[nőiMaxIdx][VersenyIdőIndex]}\n" +
+                $"\tFérfiak {adatok[férfiMaxIdx][NévIndex]} ({adatok[férfiMaxIdx][RajtszámIndex]}.) - {adatok[férfiMaxIdx][VersenyIdőIndex]}");
         }
     }
 }
