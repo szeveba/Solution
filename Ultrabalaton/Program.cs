c3c9827356c0f39b2ea349dd344d49974b01d0ed
Szegedi Barnabás <szeveba@gmail.com>
2023-12-04 16:38:56 +0100

    Project init

---
+++ Ultrabalaton/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ultrabalaton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
